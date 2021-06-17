874b2adf6bc30b70cf460e54fec258ea974fb775
Srijit Das <srijit29032001@gmail.com>
2021-06-17 15:30:06 +0530

    salience section added

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 require "./partials/header.php"
 ?>
 
-<div class="hero-container">
+<!--  Hero section starts  -->
+
+<div class="section hero-container">
   <div class="hero-content-container">
     <h3>India’s First </h3>
     <h1>Virtual Veterinary <br> Clinic</h1>
@@ -13,12 +15,16 @@ require "./partials/header.php"
     <button class="hero-cta">ENQUIRE NOW</button>
   </div>
 
+  <!--  Hero carousel starts  -->
+
     <div class="splide">
       <div class="splide__track">
         <div class="splide__list"> 
+            <!--  slide 1  -->
           <div class="splide__slide slides slide-1">
             <h2>FIRST LIVESTOCK <span>HEALTH CARE</span> ADVISORY ONLINE PLATFORM IN INDIA</h2>
           </div>
+          <!--  slide 2  -->
           <div class="splide__slide slides slide-2">
             <h2>RECEIVE THIS SPECIALITY SERVICE <span>AT NO COST</span></h2>
           </div> 
@@ -26,6 +32,40 @@ require "./partials/header.php"
       </div>
     </div>
   </div>
+
+<!--  Hero carousel ends  -->
+
+<!--  Hero section enda  -->
+
+<div class="section salience-container">
+  <div class="section-heading-container">
+    <h2 class="section-heading">Dr Agrivet – Salience</h2>
+    <div class="section-heading-underline"></div>
+  </div>
+
+  <div class="salience-content-container">
+    <div class="salience-card salience-card-1">
+      <p>Dr. AgriVet is India’s first Online Livestock Health Care Advisory Platform</p>
+    </div>
+    <div class="salience-card salience-card-2">
+      <p>Technology driven, Non-Physical, Real Time, Expert Health Care advisory for Livestock</p>
+    </div>
+    <div class="salience-card salience-card-3">
+      <p>An Online Platform that connects the Grassroot Level Farmers with the Veterinarians </p>
+    </div>
+    <div class="salience-card salience-card-4">
+      <p>An initiative that champions inclusivity by providing digital health</p>
+    </div>
+    <div class="salience-card salience-card-5">
+      <p>discover all the information you need for livestock health management.</p>
+    </div>
+    <div class="salience-card salience-card-6">
+      <p>The farmers receive this specialty services at NO COST</p>
+    </div>
+  </div>
+</div>
+
+
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
