d23d087d11e4e624e336246cca8a50b78944c709
Srijit Das <srijit29032001@gmail.com>
2021-06-25 17:12:08 +0530

    database connected

--- includes/api/form-handler-api.php
+++ includes/api/form-handler-api.php
@@ -16,9 +16,36 @@ $en_email = mysqli_real_escape_string($conn, $data['en_email']);
 $en_details = mysqli_real_escape_string($conn, $data['en_details']);
 
 
-mail($en_email,"My subject",$msg);
+if (empty($en_name) || empty($en_details) || empty($en_email) || empty($en_details)) {
+    echo json_encode(array('message' => "empty", "status" => 400));
+}
 
+elseif (!filter_var($en_email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9_ -]/", $en_name)) {
+    echo json_encode(array('message' => "invalid", "status" => 400));
+}
 
+elseif (!filter_var($en_email, FILTER_VALIDATE_EMAIL)) {
+    echo json_encode(array('message' => "email" , "status" => 400));
+}
+
+elseif (!preg_match("/^[a-zA-Z -]/", $en_name)) {
+    echo json_encode(array('message' => "name", "status" => 400));
+}
+
+else {
+    $sql = "INSERT INTO enquiry_table(u_name, mob_no, email, details) VALUES (?, ?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo json_encode(array('message' => "not", "status" => 500));
+    }
+
+    else {
+        mysqli_stmt_bind_param($stmt, "ssss", $en_name, $en_number, $en_email, $en_details);
+        mysqli_stmt_execute($stmt);
+        echo json_encode(array('name' => "{$en_name}", 'number' => "{$en_number}", 'email' => "{$en_email}", 'details' => "{$en_details}",  "status" => 201));
+    }
+}
 
 
 
